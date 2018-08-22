15fde63018a83ac0e3e9f5bfea7096b8af4d761a
Lichi Chen <lichi.chen@104.com.tw>
2018-08-22 10:48:15 +0800

    Work

--- src/index.js
+++ src/index.js
@@ -17,7 +17,21 @@ function validator(filename) {
       // Law 2
       const { total: law2Count } = passLaw2Count(document);
       console.log(`There are ${law2Count} <a> tag without rel attribute.`);
-      
+      // Law 3.1
+      const { total: law3TitleCount } = passHaveTitle(document);
+      console.log(`This HTML has ${law3TitleCount} <title> tag`);
+      // Law 3.2
+      const { total: law3DescriptionsCount } = passMetaDescriptions(document);
+      console.log(`This HTML has ${law3DescriptionsCount} <meta name="descriptions"> tag in <head>`);
+      // Law 3.3
+      const { total: law3KeywordsCount } = passMetaKeywords(document);
+      console.log(`This HTML has ${law3KeywordsCount} <meta name="keywords"> tag in <head>`);
+      // Law 4
+      const { total: strongCount } = passStrong(document);
+      console.log(`This HTML has ${strongCount} <strong> tag`);
+      // Law 5
+      const { total: h1Count } = passH1(document);
+      console.log(`This HTML has ${h1Count} <h1> tag`);
     }).catch(err => console.log(err));
 
   return 200;
@@ -39,5 +53,31 @@ function passLaw2Count(document) {
   };
 }
 
+function passHaveTitle(document) {
+  const titleNodes = document.querySelectorAll('head>title');
+  return {
+    total: titleNodes.length,
+  };
+}
+
+function passMetaDescriptions(document) {
+  const nodes = document.querySelectorAll('head>meta[name="descriptions"]');
+  return { total: nodes.length };
+}
+
+function passMetaKeywords(document) {
+  const nodes = document.querySelectorAll('head>meta[name="keywords"]');
+  return { total: nodes.length };
+}
+
+function passStrong(document) {
+  const nodes = document.querySelectorAll('strong');
+  return { total: nodes.length };
+}
+
+function passH1(document) {
+  const nodes = document.querySelectorAll('h1');
+  return { total: nodes.length };
+}
 
 module.exports = { validator };
